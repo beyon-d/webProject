e1d8674e3c2bab7bf57d0580dce60e3c0ef99d83
beyon-d <96542324+beyon-d@users.noreply.github.com>
2022-01-23 23:04:18 +0300

    starter file added

---
+++ aaitsof/src/main/java/com/webproject/aaitsof/AaitsofApplication.java
@@ -0,0 +1,13 @@
+package com.webproject.aaitsof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaitsofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaitsofApplication.class, args);
+	}
+
+}
